4abde613584f3b4eaf66f012cd63fe42ef3449f8
ejones29 <jonesew0509@gmail.com>
2025-12-04 08:43:51 -0800

    centers container class and adds Link to header logo

--- gatsby-app/src/components/Logo/Logo.tsx
+++ gatsby-app/src/components/Logo/Logo.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import FunkyChickenLogo from "../../assets/funky-chicken-logo.svg";
 import * as styles from "./Logo.module.css";
+import {Link} from "gatsby";
 
 export default function Logo() {
   return (
     <div className={styles.logoContainer}>
-      
+      <Link to="/">
         <FunkyChickenLogo />
-      
+      </Link>
     </div>
   );
 }
